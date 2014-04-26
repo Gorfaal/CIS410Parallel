d378dbdc647050b0652606172fbf1460085ffed7
Mikey Knowles <mikey@ubuntu.(none)>
2014-04-26 13:14:02 -0700

    add tbb cilk lab5

---
+++ labs/lab5/cilk_pass_init.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <cilk/cilk.h>
+#include <openssl/md5.h>
+
+const char* chars="0123456789";
+
+// tests if a hash matches a candidate password
+int test(const char* passhash, const char* passcandidate) {
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    
+    MD5((unsigned char*)passcandidate, strlen(passcandidate), digest);
+    
+    char mdString[34];
+    mdString[33]='\0';
+    int i = 0;
+    for(i=0; i<16; i++) {
+        sprintf(&mdString[i*2], "%02x", (unsigned char)digest[i]);
+    }
+    return strncmp(passhash, mdString, strlen(passhash));
+}
+
+// maps a PIN to a string
+void genpass(long passnum, char* passbuff) {
+    passbuff[8]='\0';
+    int charidx;
+    int symcount=strlen(chars);
+    int i = 0;
+    for(i=7; i>=0; i--) {
+        charidx=passnum%symcount;
+        passnum=passnum/symcount;
+        passbuff[i]=chars[charidx];
+    }
+}
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        printf("Usage: %s <password hash>\n",argv[0]);
+        return 1;
+    }
+
+   
+    char result[9];
+    long currpass=0;
+    int notfound=1;
+    int i = 0;
+    int limit=100000000;
+
+    cilk_for(i = 0; i < limit; i++) {
+	char passmatch[9];
+        genpass(i,passmatch);
+        // printf("%s\n",passmatch);
+	if(!test(argv[1], passmatch)) {
+		genpass(i,result);
+	}	
+    }
+    printf("found: %s\n",result);
+    return 0;
+}
